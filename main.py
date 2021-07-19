f3b7b460c51af1c0b76bdb9e4160d0fe571e34a0
Иващенко Максим <maxx.ivch@gmail.com>
2021-07-19 15:03:30 +0700

    n_sBranch2

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-print(func(5))
-print(prn('Hello'))
+
+print(func(30))
+print(prn('Hello World'))
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
