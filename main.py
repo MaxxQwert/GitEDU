1703233a2ed24740afedb89d906fe96a83305d00
Иващенко Максим <maxx.ivch@gmail.com>
2021-07-19 15:05:40 +0700

    master2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-print(prn('Hello'))
+print(prn('Hello? helo? helo'))
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
