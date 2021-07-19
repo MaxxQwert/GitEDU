3b7c30ee9714d0f8c5e77f2c3889afe1a5bdbf3d
Иващенко Максим <maxx.ivch@gmail.com>
2021-07-19 15:13:58 +0700

    New Files2

--- OStest2.py
+++ OStest2.py
@@ -2,5 +2,5 @@ import os
 pypath = sorted(dirpath + filename for dirpath, dirnames, filenames in os.walk("main") for filename in filenames if filename.endswith('py'))
 with open('pypath.txt', 'w') as f:
     for i in pypath:
-        print(i)
+        print(i, 'Hello')
         f.write(i + '\n')
