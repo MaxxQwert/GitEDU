527df905f8ecf9f4cf20d7ca17da90cf4194dba4
Иващенко Максим <maxx.ivch@gmail.com>
2021-08-03 17:35:31 +0700

    Asinc Started

---
+++ asynco2.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+async def foo():
+    return 42
+
+
+async def bar():
+    return 55
+
+
+async def main():
+    task = set()
+    task.add(asyncio.create_task(foo()))
+    task.add(asyncio.create_task(bar()))
+    done, pending = await asyncio.wait(task)
+
+    print(done)
+    print(pending)
+    print(task)
+
+
+asyncio.run(main())
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from include import prn
-from function import func
+import pandas as pd
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-print(prn('Hello? helo? helo'))
-print(func(300))
-print(prn('Hello World'))
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# stud = pd.read_csv('https://stepik.org/media/attachments/course/4852/StudentsPerformance.csv')
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_colwidth', None)
+# pd.set_option('display.width', 300)
+# all =(stud['lunch'].shape[0])
+# sel = (stud.loc[stud['lunch'] == 'free/reduced']['lunch'].shape[0])
+# print(stud['lunch'].value_counts(normalize=True)['free/reduced'])
+workers = [['Ivan', 'Ivanov', 100000, 2], ['Petr', 'Petrov', 150000, 2], ['Sidor', 'Sidorov', 200000, 3]]
+for worker in workers:
+    if worker[3] < 2:
+        print(f'{worker[0]} {worker[1]} is junior')
+    elif worker[3] >= 2:
+        print(f'{worker[0]} {worker[1]} is middle')
+    elif worker[3] > 5:
+        print(f'{worker[0]} {worker[1]} is senior')
